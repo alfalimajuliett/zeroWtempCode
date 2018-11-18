6d0d2cc626fa250b1e1a1637410684e21ddde56c
M. Marek-Spartz <patt0335@umn.edu>
2018-11-17 20:00:10 -0600

    initial temp update code from sensor

---
+++ temp_update_code.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import glob
+import time
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES': # strip returns a copy of the string with both leading and trailing characters removed
+        time.sleep(0.2) #code pauses for this amount of secs
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+# Lines[1] means we’re looking at the 2nd element in the array,
+#in this case, the 2nd line. Once we have the line we simply
+#get all the numbers that are after the t= this is done here
+#lines[1][equals_pos+2:]. Equals_pos is the start position
+#of the temperature (t), and we add 2 to the position, so
+#we only get the actual temperature numbers.
+
+def changes_in_repo():
+    return subprocess.call("git diff --exit-code --quiet".split()) != 0
+
+    os.chdir(REPO_DIR)
+
+    if not os.path.isdir(REPO_DIR + os.sep + ".git"):
+      subprocess.call("git init".split())
+      with open(".gitignore", "w") as f:
+        f.write("*\n")
+      subprocess.call("git add -f .gitignore pending.data completed.data".split())
+      subprocess.call(["git", "commit", "-mInitial log"])
+
+    if changes_in_repo():
+      subprocess.call("git commit -a".split() + ["-m" + c['args']])
+
+      # Only push every 2 minutes:
+      stdout, _ = subprocess.Popen("git log origin/master.. --oneline --before=2minutes".split(), stdout=subprocess.PIPE).communicate()
+      if stdout and False:
+      subprocess.call(["git", "push"])
+
+if __name__ == '__main__':
